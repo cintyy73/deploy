d863b08a64fccc0e12659fa77ecd9440e0f9e52f
cintyy73 <cintiapaezdm@gmail.com>
2024-11-25 09:46:28 -0300

    base

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Center, Heading } from "@chakra-ui/react"
+
+function App() {
+
+  return (
+  
+      <Center minH="100vh">
+        <Heading>Vite +  chakra-v02</Heading>
+      </Center>
+      
+  )
+}
+
+export default App
